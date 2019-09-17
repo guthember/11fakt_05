70b385eac7491312365b3cac46ffadb9b1fe88d9
Erik Gúth <guthember@gmail.com>
2019-09-17 15:09:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a;
+      int b;
+      double eredmeny; // lebegőpontos szám, törtszám
+
+      Console.Write("First number: ");
+      a = Convert.ToInt32( Console.ReadLine());
+
+      Console.Write("Second number: ");
+      b = Convert.ToInt32(Console.ReadLine());
+
+      eredmeny = a + b;
+      Console.WriteLine(a + "+" + b + "=" + eredmeny);
+      eredmeny = a - b;
+      Console.WriteLine(a + "-" + b + "=" + eredmeny);
+      eredmeny = a * b;
+      Console.WriteLine(a + "*" + b + "=" + eredmeny);
+      eredmeny = (double) a / b;
+      Console.WriteLine(a + "/" + b + "=" + eredmeny);
+
+      Console.ReadKey();
+    }
+  }
+}
